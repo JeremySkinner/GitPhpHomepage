24fa20024094386306ca57f2915e4599a47611bb
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2010-09-12 11:11:41 +0100

    Minor

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 $config = array(
 	"GIT_EXECUTABLE" => "\"C:\\program files (x86)\\git\\bin\\git.exe\"",
-	"REPOSITORY_ROOT" => "C:\Repositories"
-	"CHECK_DIRS_ARE_REPOS" => true
+	"REPOSITORY_ROOT" => "C:\\Repositories",
+	"CHECK_DIRS_ARE_REPOS" => true,
 );
 
 ?>
